7918b412d2d841027ee2eed92882b0639349af55
root <root@socientize.lsd.ufcg.edu.br>
2015-12-16 15:44:55 -0300

    Fixing minor bugs

--- settings_local.py
+++ settings_local.py
@@ -1,5 +1,5 @@
 # Google Drive Configuration
-GOOGLE_JSON_URL = "aguanossa-0456fcf19d82.json"
+GOOGLE_JSON_URL = "/home/aguanossa/aguanossa-0456fcf19d82.json"
 GOOGLE_DRIVE_SHEET_ID = "1Y4RXKYvdFKCtf6RMyQCjSS7iN-5AfyedfTbJa_vDO5g"
 
 
